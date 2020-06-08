c7b7d771c16ce94b793c8c5eda2ff0ccb84d8437
liudekang <1530383507@qq.com>
2020-06-08 21:22:13 +0800

    'mg'

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,15 +4,11 @@
  * @Last Modified by: mikey.liudekang
  * @Last Modified time: 2019-11-24 20:01:12
  */
-import React from 'react'
+import React from 'react';
 
-import { Button } from 'antd'
-import Graphin, { Utils } from '@antv/graphin';
-import { Toolbar } from '@antv/graphin-components';
+import { Button } from 'antd';
 
-import '@antv/graphin/dist/index.css'; // Graphin CSS
-import '@antv/graphin-components/dist/index.css'; // Graphin 组件 CSS
-// import RouterView from '../../router/RouterView';
+import styles from './index.css'
 
 export default class Home extends React.Component {
   constructor(props) {
@@ -23,19 +19,11 @@ export default class Home extends React.Component {
   asdasd = () => ('123')
 
   render = () => {
-    const aa = '123'
-    const data = Utils.mock(10).circle().graphin();
-    console.log(2888, Toolbar)
     return (
-      <div className='App' >
+      <div className={styles.homeWrap} >
         <h5>当前首页</h5>
-        <Button>annu</Button> {aa}
+        <Button>annu</Button>
 
-        <div className='App'>
-          <Graphin data={data} layout={{ name: 'concentric', }}>
-            {/* <Toolbar /> */}
-          </Graphin>
-        </div>
       </div>
     )
   }
